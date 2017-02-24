1a1426761231018c51debe191455442f905c9fc2
tasconajoey <joey.tascona@gmail.com>
2017-02-24 18:40:06 -0500

    Add all the functionality needed so far will now work on styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'images#index'
   resources :images
   devise_for :users
+  resources :users, only: [:show, :index]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
