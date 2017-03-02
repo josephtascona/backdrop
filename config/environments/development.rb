4448cd51ad7f9606fcee1c33069a547707276b6f
tasconajoey <joey.tascona@gmail.com>
2017-03-01 20:22:49 -0500

    Fix error with sending passwords

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,8 +26,10 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -51,6 +53,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
