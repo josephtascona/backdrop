2cb36535b0299876bf58c45495a425c9e7b289bf
tasconajoey <joey.tascona@gmail.com>
2017-03-02 14:20:37 -0500

    Change from gmail mailer to outlook

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,13 +71,13 @@ Rails.application.configure do
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: "smtp-mail.outlook.com",
     port: 587,
-    domain: ENV["GMAIL_DOMAIN"],
+    domain: ENV["OUTLOOK_DOMAIN"],
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: ENV["OUTLOOK_USERNAME"],
+    password: ENV["OUTLOOK_PASSWORD"]
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
